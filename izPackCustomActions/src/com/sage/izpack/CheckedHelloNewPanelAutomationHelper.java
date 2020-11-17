1030a51dd314ac12436160167e6e67fa6370d869
Franck Depoortere <franck.depoortere@sage.com>
2020-11-17 11:17:25 +0100

    X3-222465: Setup Console using izpack 5.1

---
+++ izPackCustomActions/src/com/sage/izpack/CheckedHelloNewPanelAutomationHelper.java
@@ -0,0 +1,12 @@
+package com.sage.izpack;
+
+import com.izforge.izpack.installer.automation.PanelAutomationHelper;
+
+public class CheckedHelloNewPanelAutomationHelper extends PanelAutomationHelper {
+
+	
+	public CheckedHelloNewPanelAutomationHelper()
+    {
+        super();
+    }
+}

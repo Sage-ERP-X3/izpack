55d0a752f00e2904fcc19afd3156e3f256cd57d8
Franck Depoortere <franck.depoortere@sage.com>
2021-03-08 11:31:27 +0100

    X3-237732 : Setups issues with Console 2.49 due to Izpack 5.1 Add com.sage.izpack.RegistryUninstallerNewListener

--- izPackCustomActions/src/com/sage/izpack/CheckedHelloNewConsolePanel.java
+++ izPackCustomActions/src/com/sage/izpack/CheckedHelloNewConsolePanel.java
@@ -4,22 +4,83 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.izforge.izpack.api.data.InstallData;
+import com.izforge.izpack.api.data.Variables;
 import com.izforge.izpack.api.exception.NativeLibException;
 import com.izforge.izpack.api.handler.Prompt;
 import com.izforge.izpack.core.os.RegistryDefaultHandler;
 import com.izforge.izpack.installer.console.ConsolePanel;
 import com.izforge.izpack.installer.panel.PanelView;
 import com.izforge.izpack.panels.checkedhello.CheckedHelloConsolePanel;
+import com.izforge.izpack.panels.checkedhello.RegistryHelper;
+import com.izforge.izpack.util.Console;
+
 
 public class CheckedHelloNewConsolePanel extends CheckedHelloConsolePanel {
 
 	private static Logger logger = Logger.getLogger(CheckedHelloNewConsolePanel.class.getName());
+    private final RegistryHelper registryHelper;
+    /**
+     * The prompt.
+     */
+    private final Prompt prompt;
+    /**
+     * Determines if the application is already installed.
+     */
+    private boolean registered;
+    private InstallData installData;
 
+    
 	public CheckedHelloNewConsolePanel(RegistryDefaultHandler handler, InstallData installData, Prompt prompt,
 			PanelView<ConsolePanel> panel) throws NativeLibException {
 		super(handler, installData, prompt, panel);
-		// TODO Auto-generated constructor stub
+        registryHelper = new RegistryHelper(handler, installData);
+        this.prompt = prompt;
+        registered = registryHelper.isRegistered();
+        this.installData = installData;
+
 	}
 	
+	 /**
+     * Runs the panel using the specified console.
+     *
+     * @param installData the installation data
+     * @param console     the console
+     * @return <tt>true</tt> if the panel ran successfully, otherwise <tt>false</tt>
+     */
+    @Override
+    public boolean run(InstallData installData, Console console)
+    {
+        printHeadLine(installData, console);
 
+        boolean result = true;
+        if (registered)
+        {
+            result = multipleInstall(installData);
+            if (result)
+            {
+                try
+                {
+                    registryHelper.updateUninstallName();
+                    registered = false;
+                }
+                catch (NativeLibException exception)
+                {
+                    result = false;
+                    logger.log(Level.SEVERE, exception.getMessage(), exception);
+                }
+            }
+        }
+		// We had to override this method to remove APP_VER
+		// return variables.get("APP_NAME") + " " + variables.get("APP_VER");
+    	Variables variables = this.installData.getVariables();		
+        installData.setVariable("UNINSTALL_NAME", variables.get("APP_NAME"));        
+        // installData.setVariable("UNINSTALL_NAME", registryHelper.getUninstallName());
+        if (result)
+        {
+            display(installData, console);
+            result = promptEndPanel(installData, console);
+        }
+
+        return result;
+    }
 }
--- izPackCustomActions/src/com/sage/izpack/CheckedHelloNewPanelAutomationHelper.java
+++ izPackCustomActions/src/com/sage/izpack/CheckedHelloNewPanelAutomationHelper.java
@@ -1,12 +1,52 @@
 package com.sage.izpack;
 
-import com.izforge.izpack.installer.automation.PanelAutomationHelper;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class CheckedHelloNewPanelAutomationHelper extends PanelAutomationHelper {
+import com.izforge.izpack.api.adaptator.IXMLElement;
+import com.izforge.izpack.api.data.InstallData;
+import com.izforge.izpack.api.data.Variables;
+import com.izforge.izpack.api.exception.NativeLibException;
+import com.izforge.izpack.core.os.RegistryDefaultHandler;
+import com.izforge.izpack.panels.checkedhello.CheckedHelloPanelAutomationHelper;
+import com.izforge.izpack.panels.checkedhello.RegistryHelper;
 
-	
-	public CheckedHelloNewPanelAutomationHelper()
+public class CheckedHelloNewPanelAutomationHelper extends CheckedHelloPanelAutomationHelper {
+
+	private static Logger logger = Logger.getLogger(CheckedHelloNewPanelAutomationHelper.class.getName());
+
+	private final RegistryHelper registryHelper;
+    private final boolean registered;
+    private InstallData installData;
+    
+	public CheckedHelloNewPanelAutomationHelper(RegistryDefaultHandler handler, InstallData installData) throws NativeLibException
     {
-        super();
+        super(handler, installData);
+        this.registryHelper = new RegistryHelper(handler, installData);
+        this.registered = registryHelper.isRegistered();
+        this.installData = installData;
+        
+        String path = registryHelper.getInstallationPath();
+		if (path != null) {
+			installData.setVariable("TargetPanel.dir.windows", path);
+			logger.log(Level.FINE, "Set TargetPanel.dir.windows: " + path);
+
+			installData.setVariable(InstallData.INSTALL_PATH, path);
+			logger.log(Level.FINE, "Set INSTALL_PATH", path);
+		}
     }
+	
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void runAutomated(InstallData installData, IXMLElement panelRoot)
+    {
+		// We had to override this method to remove APP_VER
+		// return variables.get("APP_NAME") + " " + variables.get("APP_VER");
+    	Variables variables = this.installData.getVariables();		
+        installData.setVariable("UNINSTALL_NAME", variables.get("APP_NAME"));
+    }
+
 }

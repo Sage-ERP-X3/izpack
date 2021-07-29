eb070a7260c41ef2450d3abd0bc7d6dae904c8e4
Franck Depoortere <franck.depoortere@sage.com>
2021-07-29 12:23:10 +0200

    X3-251676 : IZPACK 5.1.3

--- izPackCustomActions/src/com/sage/izpack/TargetNewPanel.java
+++ izPackCustomActions/src/com/sage/izpack/TargetNewPanel.java
@@ -2,7 +2,6 @@ package com.sage.izpack;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import com.izforge.izpack.api.data.Panel;
 import com.izforge.izpack.api.resource.Resources;
 import com.izforge.izpack.gui.log.Log;
@@ -30,25 +29,51 @@ public class TargetNewPanel extends TargetPanel {
 		logger.log(Level.FINE, "TargetNewPanel.panelActivate  Path: " + pathSelectionPanel.getPath());
 	}
 
-	// TODO: FRDEPO =>  How to set variable ${component.node.name} in a generic way ?
-	// <variable name="TargetPanel.dir.windows" value="c:\Sage\SafeX3\${component.node.name}" condition="!updatemode"/>
+	// TODO: FRDEPO => How to set variable ${component.node.name} in a generic way ?
+	// <variable name="TargetPanel.dir.windows"
+	// value="c:\Sage\SafeX3\${component.node.name}" condition="!updatemode"/>
 
 	@Override
 	public void saveData() {
 
 		logger.log(Level.FINE, "TargetNewPanel.saveData");
+
+		// <ComponentName>c:\Sage\SafeX3\${component.node.name}</ComponentName>
+		String platformSep = "\\\\"; // "\"[\\/]+\"";
+		int positionFromEnd = 0;
+		String variableNameRaw = getMetadata().getConfigurationOptionValue("SetVariableName", installData.getRules());
+		String variableName = null;
+		logger.log(Level.FINE, "TargetNewPanel.saveData  variableNameRaw: " + variableNameRaw);
+
+		if (variableNameRaw != null && variableNameRaw.indexOf("${") >= 0 && variableNameRaw.indexOf("}") >= 0) {
+
+			variableName = variableNameRaw.substring(variableNameRaw.indexOf("${") + 2, variableNameRaw.indexOf("}"));
+
+			String[] pathElements = variableNameRaw.split(platformSep);
+			for (int i = 0; i < pathElements.length; i++) {
+				String part = pathElements[i];
+				if (part.equals(variableName)) {
+					positionFromEnd = pathElements.length - 1 - i;
+				}
+			}
+			logger.log(Level.FINE, "TargetNewPanel.saveData  variableName: '" + variableName + "'  positionFromEnd: "
+					+ positionFromEnd);
+		}
+
 		super.saveData();
 		String path = pathSelectionPanel.getPath();
 
-        // String show = pathSelectionPanel. getConfigurationOptionValue("", installData.getRules());        
-		// installData.getPlatform().
-		
 		logger.log(Level.FINE, "TargetNewPanel.saveData  Path: " + path);
-		if (path.lastIndexOf("\\") > 0) {
-			String name = path.substring(path.lastIndexOf("\\") + 1);
-			installData.setVariable("component.node.name", name);
-			logger.log(Level.FINE, "TargetNewPanel.saveData  component.node.name: " + name);
-		}
+		if (variableName != null) {
+
+			String[] pathElements = path.split(platformSep);
+			String variableValue = pathElements[(pathElements.length - 1 - positionFromEnd)];
+			installData.setVariable(variableName, variableValue);
+			logger.log(Level.FINE, "TargetNewPanel.saveData path: " + path + "  setVariable  '" + variableName + "': "
+					+ variableValue);
+			
+			}				
+		
 	}
 
 }

87ba0da25315b6098fcde7aa14267b06bc517d29
Franck Depoortere <franck.depoortere@sage.com>
2021-08-04 09:29:42 +0200

    X3-251676 : IZPACK 5.1.3

---
+++ izPackCustomActions/src/com/sage/izpack/TargetNewPanelAutomationHelper.java
@@ -0,0 +1,13 @@
+package com.sage.izpack;
+
+import com.izforge.izpack.panels.target.TargetPanelAutomation;
+
+public class TargetNewPanelAutomationHelper extends TargetPanelAutomation {
+
+	public TargetNewPanelAutomationHelper() {
+		super();
+		
+	}
+	
+	
+}

0402a9a4dd9adce0c2ec876ea8fafd4651c92e32
Franck Depoortere <franck.depoortere@sage.com>
2021-10-27 19:26:50 +0200

    X3-250275 : move prerequisites resources within the project Fix WinRegistry Java11API

--- src/lib/com/izforge/izpack/util/WinRegistry.java
+++ src/lib/com/izforge/izpack/util/WinRegistry.java
@@ -39,43 +39,74 @@ public class WinRegistry {
 	private static Method regDeleteKey = null;
 	private static Method regDeleteValue = null;
 
+	private static boolean Java11API = false;
+	
 	static {
 
 		try {
 			try {
-				// In Java 11, the handles within WindowsPreferences were changed from int to long. 
-				// This change was made here in OpenJDK11. You'll have to change the method signatures to make this code run in a Java 11+ environment
+				// In Java 11, the handles within WindowsPreferences were changed from int to
+				// long.
+				// This change was made here in OpenJDK11. You'll have to change the method
+				// signatures to make this code run in a Java 11+ environment
 				// https://stackoverflow.com/questions/65630568/java-error-java-util-prefs-windowspreferences-windowsregopenkey
 				regOpenKey = userClass.getDeclaredMethod("WindowsRegOpenKey",
 						new Class[] { long.class, byte[].class, int.class });
+				regOpenKey.setAccessible(true);
+				regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", new Class[] { long.class });
+				regCloseKey.setAccessible(true);
+				regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx",
+						new Class[] { long.class, byte[].class });
+				regQueryValueEx.setAccessible(true);
+				regEnumValue = userClass.getDeclaredMethod("WindowsRegEnumValue",
+						new Class[] { long.class, int.class, int.class });
+				regEnumValue.setAccessible(true);
+				regQueryInfoKey = userClass.getDeclaredMethod("WindowsRegQueryInfoKey1", new Class[] { long.class });
+				regQueryInfoKey.setAccessible(true);
+				regEnumKeyEx = userClass.getDeclaredMethod("WindowsRegEnumKeyEx",
+						new Class[] { long.class, int.class, int.class });
+				regEnumKeyEx.setAccessible(true);
+				regCreateKeyEx = userClass.getDeclaredMethod("WindowsRegCreateKeyEx",
+						new Class[] { long.class, byte[].class });
+				regCreateKeyEx.setAccessible(true);
+				regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx",
+						new Class[] { long.class, byte[].class, byte[].class });
+				regSetValueEx.setAccessible(true);
+				regDeleteValue = userClass.getDeclaredMethod("WindowsRegDeleteValue",
+						new Class[] { long.class, byte[].class });
+				regDeleteValue.setAccessible(true);
+				regDeleteKey = userClass.getDeclaredMethod("WindowsRegDeleteKey", new Class[] { long.class, byte[].class });
+				Java11API = true;
 			} catch (Exception e) {
+				// Java < 11
 				regOpenKey = userClass.getDeclaredMethod("WindowsRegOpenKey",
 						new Class[] { int.class, byte[].class, int.class });
+				regOpenKey.setAccessible(true);
+				regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", new Class[] { int.class });
+				regCloseKey.setAccessible(true);
+				regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx",
+						new Class[] { int.class, byte[].class });
+				regQueryValueEx.setAccessible(true);
+				regEnumValue = userClass.getDeclaredMethod("WindowsRegEnumValue",
+						new Class[] { int.class, int.class, int.class });
+				regEnumValue.setAccessible(true);
+				regQueryInfoKey = userClass.getDeclaredMethod("WindowsRegQueryInfoKey1", new Class[] { int.class });
+				regQueryInfoKey.setAccessible(true);
+				regEnumKeyEx = userClass.getDeclaredMethod("WindowsRegEnumKeyEx",
+						new Class[] { int.class, int.class, int.class });
+				regEnumKeyEx.setAccessible(true);
+				regCreateKeyEx = userClass.getDeclaredMethod("WindowsRegCreateKeyEx",
+						new Class[] { int.class, byte[].class });
+				regCreateKeyEx.setAccessible(true);
+				regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx",
+						new Class[] { int.class, byte[].class, byte[].class });
+				regSetValueEx.setAccessible(true);
+				regDeleteValue = userClass.getDeclaredMethod("WindowsRegDeleteValue",
+						new Class[] { int.class, byte[].class });
+				regDeleteValue.setAccessible(true);
+				regDeleteKey = userClass.getDeclaredMethod("WindowsRegDeleteKey", new Class[] { int.class, byte[].class });
 			}
-			regOpenKey.setAccessible(true);
-			regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", new Class[] { int.class });
-			regCloseKey.setAccessible(true);
-			regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx",
-					new Class[] { int.class, byte[].class });
-			regQueryValueEx.setAccessible(true);
-			regEnumValue = userClass.getDeclaredMethod("WindowsRegEnumValue",
-					new Class[] { int.class, int.class, int.class });
-			regEnumValue.setAccessible(true);
-			regQueryInfoKey = userClass.getDeclaredMethod("WindowsRegQueryInfoKey1", new Class[] { int.class });
-			regQueryInfoKey.setAccessible(true);
-			regEnumKeyEx = userClass.getDeclaredMethod("WindowsRegEnumKeyEx",
-					new Class[] { int.class, int.class, int.class });
-			regEnumKeyEx.setAccessible(true);
-			regCreateKeyEx = userClass.getDeclaredMethod("WindowsRegCreateKeyEx",
-					new Class[] { int.class, byte[].class });
-			regCreateKeyEx.setAccessible(true);
-			regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx",
-					new Class[] { int.class, byte[].class, byte[].class });
-			regSetValueEx.setAccessible(true);
-			regDeleteValue = userClass.getDeclaredMethod("WindowsRegDeleteValue",
-					new Class[] { int.class, byte[].class });
-			regDeleteValue.setAccessible(true);
-			regDeleteKey = userClass.getDeclaredMethod("WindowsRegDeleteKey", new Class[] { int.class, byte[].class });
+
 			regDeleteKey.setAccessible(true);
 		} catch (Exception e) {
 			Debug.log("Cannot init WinRegistry : " + e.getMessage());
@@ -97,12 +128,23 @@ public class WinRegistry {
 	 */
 	public static String valueForKey(int hkey, String path, String key)
 			throws IllegalArgumentException, IllegalAccessException, InvocationTargetException, IOException {
+		
+		if (Java11API) {
+			if (hkey == HKEY_LOCAL_MACHINE)
+				return valueForKeyJava11(systemRoot, hkey, path, key);
+			else if (hkey == HKEY_CURRENT_USER)
+				return valueForKeyJava11(userRoot, hkey, path, key);
+			else
+				return valueForKeyJava11(null, hkey, path, key);			
+		}
+		else {
 		if (hkey == HKEY_LOCAL_MACHINE)
 			return valueForKey(systemRoot, hkey, path, key);
 		else if (hkey == HKEY_CURRENT_USER)
 			return valueForKey(userRoot, hkey, path, key);
 		else
 			return valueForKey(null, hkey, path, key);
+		}
 	}
 
 	/**
@@ -264,6 +306,20 @@ public class WinRegistry {
 		return (valb != null ? parseValue(valb) : queryValueForKey(hkey, path, key));
 	}
 
+	private static String valueForKeyJava11(Preferences root, int hkey, String path, String key)
+			throws IllegalArgumentException, IllegalAccessException, InvocationTargetException, IOException {
+		long[] handles = (long[]) regOpenKey.invoke(root,
+				new Object[] { new Integer(hkey), toCstr(path), new Integer(KEY_READ) });
+		if (handles[1] != REG_SUCCESS)
+			throw new IllegalArgumentException(
+					"The system can not find the specified path: '" + getParentKey(hkey) + "\\" + path + "'");
+		byte[] valb = (byte[]) regQueryValueEx.invoke(root, new Object[] { new Long(handles[0]), toCstr(key) });
+		regCloseKey.invoke(root, new Object[] { new Long(handles[0]) });
+		return (valb != null ? parseValue(valb) : queryValueForKey(hkey, path, key));
+	}
+
+	
+	
 	private static String queryValueForKey(int hkey, String path, String key) throws IOException {
 		return queryValuesForPath(hkey, path).get(key);
 	}

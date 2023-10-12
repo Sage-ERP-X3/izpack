014b336f4bd6d11bf0027cf17b96677657e03059
Franck Depoortere <franck.depoortere@sage.com>
2023-10-12 13:35:40 +0200

    X3-293748: Upgrade remaining IZPack 4 installers to IZPack 5 : Adxadmin & Runtime ImplementPortValidator

--- izPackCustomActions/src/com/sage/izpack/PreValidatePacksPanelAction.java
+++ izPackCustomActions/src/com/sage/izpack/PreValidatePacksPanelAction.java
@@ -1,21 +1,96 @@
 package com.sage.izpack;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.izforge.izpack.Pack;
+import com.izforge.izpack.api.data.AutomatedInstallData;
 import com.izforge.izpack.api.data.InstallData;
-import com.izforge.izpack.api.data.Panel;
 import com.izforge.izpack.api.data.PanelActionConfiguration;
-import com.izforge.izpack.api.factory.ObjectFactory;
 import com.izforge.izpack.api.handler.AbstractUIHandler;
-import com.izforge.izpack.api.resource.Resources;
-import com.izforge.izpack.api.rules.RulesEngine;
-import com.izforge.izpack.installer.data.GUIInstallData;
-import com.izforge.izpack.installer.gui.InstallerFrame;
-import com.izforge.izpack.panels.packs.PacksPanel;
 
 public class PreValidatePacksPanelAction implements com.izforge.izpack.data.PanelAction  {
 
+	private static final Logger logger = Logger.getLogger(PreValidatePacksPanelAction.class.getName());
+    private Map installedpacks = null;
+
 	@Override
-	public void executeAction(InstallData arg0, AbstractUIHandler arg1) {
-		// FRDEPO : TODO
+	public void executeAction(InstallData installData, AbstractUIHandler arg1) {
+        // well if in modify mode
+        // then we must select already installed packs
+		
+		boolean modifyinstallation = Boolean.valueOf(installData.getVariable(InstallData.MODIFY_INSTALLATION));
+		this.installedpacks = new HashMap();
+		if (modifyinstallation)
+        {
+            // installation shall be modified
+            // load installation information
+			logger.log(Level.FINE, "Update mode, loading installed packs ...");
+			
+            try
+            {
+            	File file= new File(installData.getInstallPath() + File.separator + AutomatedInstallData.INSTALLATION_INFORMATION);
+                FileInputStream fin = new FileInputStream(file);
+                ObjectInputStream oin = new ObjectInputStream(fin);
+                List packsinstalled = (List) oin.readObject();
+                for (Object aPacksinstalled : packsinstalled)
+                {
+                    Pack installedpack = (Pack) aPacksinstalled;
+                    if ((installedpack.id != null) && (installedpack.id.length() > 0))
+                    {
+                        installedpacks.put(installedpack.id, installedpack);
+                        logger.log(Level.FINE, "Found " +installedpack.id);
+                    }
+                    else
+                    {
+                        installedpacks.put(installedpack.name, installedpack);
+                        logger.log(Level.FINE, "Found " +installedpack.name);
+                    }
+                }
+                
+                this.removeAlreadyInstalledPacks(installData.getSelectedPacks());
+                // TODO: FRDEPO
+                //   adata.installedPacks = packsinstalled;
+
+                
+                List<com.izforge.izpack.api.data.Pack> packages =  new ArrayList<com.izforge.izpack.api.data.Pack>();
+                for (Object aPack : installData.getAvailablePacks())
+                {
+                    if (installedpacks.containsKey( ((Pack)aPack).id ) || installedpacks.containsKey( ((Pack)aPack).name ) )
+                    {
+                    	((Pack)aPack).preselected = true;
+                        ((Pack)aPack).required = true;
+                    	packages.add((com.izforge.izpack.api.data.Pack)aPack);                
+                    }
+                }
+            	installData.setSelectedPacks(packages);
+
+            	logger.log(Level.FINE,"Found " + packsinstalled.size() + " installed packs");
+            	logger.log(Level.FINE,"Loading properties ...");
+                
+            	
+            } catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            catch (java.io.IOException e)
+            {
+                e.printStackTrace();
+            }
+            catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+		
 	}
 
 	@Override
@@ -24,6 +99,34 @@ public class PreValidatePacksPanelAction implements com.izforge.izpack.data.Pane
 	}
 
 	
+	private void removeAlreadyInstalledPacks(List<com.izforge.izpack.api.data.Pack> selectedpacks)
+    {
+        List<Pack> removepacks = new ArrayList<Pack>();
+
+        for (Object selectedpack1 : selectedpacks)
+        {
+            Pack selectedpack = (Pack) selectedpack1;
+            String key = "";
+            if ((selectedpack.id != null) && (selectedpack.id.length() > 0))
+            {
+                key = selectedpack.id;
+            }
+            else
+            {
+                key = selectedpack.name;
+            }
+            if (installedpacks.containsKey(key))
+            {
+                // pack is already installed, remove it
+                removepacks.add(selectedpack);
+            }
+        }
+        for (Pack removepack : removepacks)
+        {
+            selectedpacks.remove(removepack);
+        }
+    }
+    
 
 	
 	/*

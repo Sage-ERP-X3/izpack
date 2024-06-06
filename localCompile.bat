a1d4aae237b2aee8db9d61643e02d6ccf6f56fa8
Franck Depoortere <franck.depoortere@sage.com>
2024-06-06 15:39:34 +0200

    X3-315535: Installing version 7.0.11 from new install configuration

--- localCompile.bat
+++ localCompile.bat
@@ -3,7 +3,9 @@ REM Compile izpack 4.3 SAGE
 REM 
 
 REM We need JAVA 1.8 : wont compile with JAVA > 1.8
-set JAVA_HOME=C:\Program Files\java\jdk-8.0.302
+REM set JAVA_HOME=C:\Program Files\java\jdk-8.0.302
+REM 
+REM IzPack 4.3.11: has been migrated to Java 11
+set JAVA_HOME=C:\Program Files\Zulu\zulu-11
 
-cd src
-ant -file build.xml
+ant -file src\build.xml all

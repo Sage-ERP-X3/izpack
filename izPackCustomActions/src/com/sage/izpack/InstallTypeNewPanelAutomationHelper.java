f41c2a5aa98f7b5292d6aaef385772f3e44b8e6b
Łukasz Gruner <lukasz.gruner@sage.com>
2024-06-17 14:12:46 +0200

    X3-307267: fix InstallTypeNewPanel - write installation data xml

--- izPackCustomActions/src/com/sage/izpack/InstallTypeNewPanelAutomationHelper.java
+++ izPackCustomActions/src/com/sage/izpack/InstallTypeNewPanelAutomationHelper.java
@@ -3,6 +3,7 @@ package com.sage.izpack;
 import com.izforge.izpack.api.adaptator.IXMLElement;
 import com.izforge.izpack.api.adaptator.impl.XMLElementImpl;
 import com.izforge.izpack.api.data.AutomatedInstallData;
+import com.izforge.izpack.api.data.InstallData;
 import com.izforge.izpack.api.exception.InstallerException;
 import com.izforge.izpack.installer.automation.PanelAutomationHelper;
 
@@ -40,23 +41,34 @@ public class InstallTypeNewPanelAutomationHelper extends PanelAutomationHelper
 
     }
 
-    public void makeXMLData(AutomatedInstallData installData, IXMLElement panelRoot)
+    public void createInstallationRecord(AutomatedInstallData installData, IXMLElement panelRoot)
     {
         // part of MODIFY_INSTALLATION
-    	InstallTypeNewPanelAutomationHelper automationHelper = new InstallTypeNewPanelAutomationHelper();
-        automationHelper.makeXMLData(installData, panelRoot);
-        
-        // part of target path
-        
-        IXMLElement ipath = new XMLElementImpl("installpath",panelRoot);
-        ipath.setContent(installData.getInstallPath());
+        IXMLElement ipath = new XMLElementImpl(InstallData.MODIFY_INSTALLATION, panelRoot);
+        // check this writes even if value is the default,
+        // because without the constructor, default does not get set.
+        if (installData.getVariable(InstallData.MODIFY_INSTALLATION) != null) {
+        	ipath.setContent(installData.getVariable(InstallData.MODIFY_INSTALLATION));
+        } else {
+        	ipath.setContent(Boolean.FALSE.toString());
+        }
 
-        IXMLElement prev = panelRoot.getFirstChildNamed("installpath");
-        if (prev != null)
-        {
-            panelRoot.removeChild(prev);
+        IXMLElement prev = panelRoot.getFirstChildNamed(InstallData.MODIFY_INSTALLATION);
+        if (prev != null) {
+        	panelRoot.removeChild(prev);
         }
         panelRoot.addChild(ipath);
+
+        // part of target path
+        IXMLElement ipath2 = new XMLElementImpl("installpath",panelRoot);
+        ipath2.setContent(installData.getInstallPath());
+
+        IXMLElement prev2 = panelRoot.getFirstChildNamed("installpath");
+        if (prev2 != null)
+        {
+            panelRoot.removeChild(prev2);
+        }
+        panelRoot.addChild(ipath2);
         
         
 
@@ -67,22 +79,48 @@ public class InstallTypeNewPanelAutomationHelper extends PanelAutomationHelper
     {
 
         // part of MODIFY_INSTALLATION
-    	InstallTypeNewPanelAutomationHelper automationHelper = new InstallTypeNewPanelAutomationHelper();
-        automationHelper.runAutomated(installData, panelRoot);
+    	IXMLElement ipath = panelRoot.getFirstChildNamed(InstallData.MODIFY_INSTALLATION);
+
+        String modify = null;
         
+        try 
+        {    
+            modify=ipath.getContent().trim();
+        }
+        catch (Exception ex)
+        {
+            // assume a normal install 
+            installData.setVariable(InstallData.MODIFY_INSTALLATION, "false");
+        }
+                 
+        if (modify == null || "".equals(modify))
+        {
+            // assume a normal install 
+            installData.setVariable(InstallData.MODIFY_INSTALLATION, "false");
+        }
+        else
+        {
+            if (Boolean.parseBoolean(modify))
+            {
+                installData.setVariable(InstallData.MODIFY_INSTALLATION, "true");
+            }
+            else
+            {
+                installData.setVariable(InstallData.MODIFY_INSTALLATION, "false");
+            }
+        } 
         // part of target path
-        IXMLElement ipath = panelRoot.getFirstChildNamed("installpath");
+        IXMLElement ipath2 = panelRoot.getFirstChildNamed("installpath");
 
         String installpath = null;
         
         try 
         {    
-            installpath=ipath.getContent().trim();
+            installpath=ipath2.getContent().trim();
             installData.setInstallPath(installpath);
             
             
             System.out.println();
-            // System.out.println(installData.langpack.getString("TargetPanel.summaryCaption"));
             System.out.println(ResourcesHelper.getCustomPropString("TargetPanel.summaryCaption"));
             System.out.println(installpath);
             System.out.println();

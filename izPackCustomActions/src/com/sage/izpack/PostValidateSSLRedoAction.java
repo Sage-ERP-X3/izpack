ad32bfb07dc5c22a76261cff7c36b724e621ec18
Łukasz Gruner <lukasz.gruner@sage.com>
2024-07-01 11:59:20 +0200

    feature(X3-293750): add needed panel actions and validators for mongodb install (#26)

---
+++ izPackCustomActions/src/com/sage/izpack/PostValidateSSLRedoAction.java
@@ -0,0 +1,32 @@
+package com.sage.izpack;
+
+import com.izforge.izpack.api.data.InstallData;
+import com.izforge.izpack.api.data.PanelActionConfiguration;
+import com.izforge.izpack.api.handler.AbstractUIHandler;
+import com.izforge.izpack.data.PanelAction;
+
+public class PostValidateSSLRedoAction implements PanelAction {
+
+	@Override
+	public void executeAction(final InstallData adata, AbstractUIHandler handler) {
+		String strRedoSSL = adata.getVariable("MONGODB.SSL.REDO");
+		String strUpdate = adata.getVariable("MODIFY.IZPACK.INSTALL");
+		String strSSLAlreadyDone = adata.getVariable("mongodb.ssl.alreadydone");
+
+		if ("true".equalsIgnoreCase(strUpdate) && "true".equalsIgnoreCase(strRedoSSL)
+				&& "true".equalsIgnoreCase(strSSLAlreadyDone)) {
+			// we want to redo ssl configuration
+			// set mongodb.ssl.alreadydone to false
+
+			adata.setVariable("mongodb.ssl.alreadydone", "false");
+
+		}
+	}
+
+	@Override
+	public void initialize(PanelActionConfiguration configuration) {
+		// nothing to initialize
+
+	}
+
+}
